c109c7539b48a1c7a63aa0099b8e4fde486a15a9
Mattias Zippert <mattias@zippert.org>
2011-07-04 15:55:20 +0200

    Init version web parser

---
+++ WebReader/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="MainWindow.xaml.cs" company="Lokaldelen AB">
+//   Mattias Z
+// </copyright>
+// <summary>
+//   Interaction logic for MainWindow.xaml
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace WebReader
+{
+    using System;
+    using System.Windows;
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainWindow"/> class.
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// </summary>
+        /// <param name="sender">
+        /// The sender.
+        /// </param>
+        /// <param name="e">
+        /// The e.
+        /// </param>
+        private void ParseButtonClick(object sender, RoutedEventArgs e)
+        {
+            var parser = new WebParser(urlField.Text);
+            string result = null;
+            try
+            {
+                result = parser.Parse();
+            }
+            catch (Exception exception)
+            {
+                result = exception.Message;
+            }
+
+            textBlock1.Text = result;
+        }
+
+
+    }
+}
---
+++ WebReader/WebParser.cs
@@ -0,0 +1,88 @@
+﻿
+
+namespace WebReader
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Net;
+    using System.Text;
+    using System.Windows;
+
+    class WebParser
+    {
+        /// <summary>
+        /// Gets or sets Url.
+        /// </summary>
+        private string Url { get; set; }
+
+        /// <summary>
+        /// Gets or sets Enc.
+        /// </summary>
+        private Encoding Enc { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WebParser"/> class.
+        /// </summary>
+        /// <param name="url">
+        /// The url to parse.
+        /// </param>
+        /// <param name="encoding">
+        /// The encoding of the input URL
+        /// </param>
+        public WebParser(string url, Encoding encoding = null)
+        {
+            this.Url = url;
+            if (encoding == null)
+            {
+                this.Enc = Encoding.UTF8;
+            }
+            else
+            {
+                this.Enc = encoding;    
+            }
+            
+        }
+
+        /// <summary>
+        /// Parses website and returns the result
+        /// </summary>
+        /// <returns>
+        /// The webresponse as a string
+        /// </returns>
+        public string Parse()
+        {
+            WebResponse response = null;
+            StreamReader reader = null;
+            string result = null;
+            try
+            {
+                var request = (HttpWebRequest)WebRequest.Create(this.Url);
+                request.Method = "GET";
+                response = request.GetResponse();
+                var respStream = response.GetResponseStream();
+                if (respStream != null)
+                {
+                    reader = new StreamReader(respStream, this.Enc);
+                    result = reader.ReadToEnd();
+                }
+            }
+            finally
+            {
+                if (reader != null)
+                {
+                    reader.Close();
+                }
+
+                if (response != null)
+                {
+                    response.Close();
+                }
+            }
+
+            return result;
+        }
+
+    }
+}
