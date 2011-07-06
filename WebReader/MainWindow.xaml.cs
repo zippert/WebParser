45e6f6164bc720ac43cfccba80aa9299deffdd70
Mattias Zippert <mattias@zippert.org>
2011-07-06 15:43:28 +0200

    Added parser logic for image URLs

--- WebReader/MainWindow.xaml.cs
+++ WebReader/MainWindow.xaml.cs
@@ -10,6 +10,7 @@
 namespace WebReader
 {
     using System;
+    using System.Collections.Generic;
     using System.Windows;
 
     /// <summary>
@@ -47,8 +48,47 @@ namespace WebReader
             }
 
             textBlock1.Text = result;
+
+            resultList.Items.Clear();
+            foreach (var item in this.LoadResults(result))
+            {
+                resultList.Items.Add(item);
+            }
+            
         }
 
+        /// <summary>
+        /// Loads the parsed result and produces the requested data
+        /// </summary>
+        /// <param name="parsedPage">
+        /// The parsed page.
+        /// </param>
+        private string[] LoadResults(string parsedPage)
+        {
+            var array = new List<string>();
 
+            // Let's parse out all <img>-tags first so we can potentially id all values later
+            var startPos = parsedPage.IndexOf("<img");
+            int endPos;
+            while (startPos != -1)
+            {
+                endPos = parsedPage.IndexOf(">", startPos);
+                array.Add(parsedPage.Substring(startPos, endPos - startPos + 1).Replace("\n", string.Empty));
+                startPos = parsedPage.IndexOf("<img", endPos + 1);
+            }
+
+            var imgs = new List<string>();
+
+            // Then we extract only the path itself for the source image
+            
+            foreach (var item in array)
+            {
+                startPos = item.IndexOf("src=\"") + 5;
+                endPos = item.IndexOf("\"", startPos + 1);
+                imgs.Add(item.Substring(startPos, endPos - startPos));
+            }
+
+            return imgs.ToArray();
+        }
     }
 }
